eeeaca1bae715d5aae0f5d9861d5a801ac8b95fd
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-11-26 20:19:03 +0800

    Initial commit

---
+++ MyModPlugin.cs
@@ -0,0 +1,50 @@
+﻿#undef DEBUG
+
+using BepInEx;
+using BepInEx.Logging;
+using R2API.Utils;
+
+namespace My.Mod.Namespace
+{
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency("SomeAPI")]
+    public class MyModPluginPlugin : BaseUnityPlugin
+    {
+        public const string ModVer =
+#if DEBUG
+            "0." +
+#endif
+            "0.0.1";
+
+        public const string ModName = "MyModName";
+        public const string ModGuid = "com.Chen.MyModName";
+
+        public static ManualLogSource _logger;
+
+        private void Awake()
+        {
+            _logger = Logger;
+
+#if DEBUG
+            Logger.LogWarning("Running test build with debug enabled!");
+            On.RoR2.Networking.GameNetworkManager.OnClientConnect += (self, user, t) => { };
+#endif
+
+            //using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ChensTemplate.mymod_assets"))
+            //{
+            //    var bundle = AssetBundle.LoadFromStream(stream);
+            //    var provider = new AssetBundleResourcesProvider("@ChensGradiusMod", bundle);
+            //    ResourcesAPI.AddProvider(provider);
+            //}
+
+            //using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ChensTemplate.mymod_soundbank.bnk"))
+            //{
+            //    var bytes = new byte[stream.Length];
+            //    stream.Read(bytes, 0, bytes.Length);
+            //    SoundAPI.SoundBanks.Add(bytes);
+            //}
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Chen's Template
+
+## Description
+
+A Risk of Rain 2 project that does nothing! It's only a template.
+
+## Installation
+
+Use **r2modman** mod manager to install this mod.
+
+## Contact
+- Issue Page: https://github.com/cheeeeeeeeeen/RoR2-ChensTemplate/issues
+- Email: `blancfaye7@gmail.com`
+- Discord: `Chen#1218`
+- RoR2 Modding Server: https://discord.com/invite/5MbXZvd
+- Give a tip through Ko-fi: https://ko-fi.com/cheeeeeeeeeen
+
+## Changelog
+
+**0.0.1**
+- Your changes here.
